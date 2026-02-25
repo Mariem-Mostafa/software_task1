cc0e5491cfbff51b9c4ed6e0fa6276ba67314012
Mariem-Mostafa <mariemmostafaahmed2@gmail.com>
2026-02-26 00:45:12 +0200

    New version

--- Program.cs
+++ Program.cs
@@ -15,22 +15,77 @@ namespace My_Frist_Code
         }
     class Program
     {
-        static void addStudent(Student st)
+        static bool idDuplicated(int ID , Student[] students, int numOfStudent , Student currentStud) 
         {
-            Console.Write("\nEnter Student ID:");
-            st.ID =int.Parse(Console.ReadLine());
+            for (int i = 0; i < numOfStudent; i++)
+            {
+                if (students[i] != currentStud && students[i].ID == ID)  
+                    return true; 
+            }
+            return false;
+        }
+        static int ReadInt(string message) {
+            int number = 0 ;
+            bool valid=false ;
+            while (!valid)
+            {
+                Console.Write(message);
+                try
+                {
+                    number = int.Parse(Console.ReadLine());
+                    valid = true;
+                }
+                catch 
+                {
+                    Console.WriteLine("\n Invalid number, try again");
+                }
+            }
+            return number ;
+        }
+        static void addStudent(Student st , Student[] students, int numOfStudent)
+        {
+            
+             while (true) 
+            {
+                 st.ID = ReadInt("\nEnter Student ID: ");
 
-            Console.Write("Enter Student Name:");
+                if (st.ID < 1000 || st.ID > 9999)
+                    Console.WriteLine("\n ID must be 4 digits");
+                else if (idDuplicated(st.ID, students, numOfStudent , st)) 
+                    Console.WriteLine("\n ID already exists"); 
+                else 
+                    break;
+
+            }
+
+            Console.Write("\nEnter Student Name:");
             st.name = Console.ReadLine();
 
-            Console.Write("Enter Student Level:");
-            st.level = int.Parse(Console.ReadLine());
-            
-            Console.Write("Enter Student Department:");
+           
+            st.level = ReadInt("\nEnter Student Level: ");
+
+            Console.Write("\nEnter Student Department:");
             st.department =Console.ReadLine();
 
-            Console.Write("Enter Student GPA:");
-            st.gpa = double.Parse(Console.ReadLine());
+            bool validDouble = false;
+            while (!validDouble)
+            {
+                Console.Write("\nEnter Student GPA:");
+                try
+                {
+                    st.gpa = double.Parse(Console.ReadLine());
+                    if (st.gpa < 0 || st.gpa > 4) 
+                        Console.WriteLine("\n GPA must be between 0 and 4"); 
+                    else
+                        validDouble = true;
+                }
+                catch
+                {
+                    Console.WriteLine("\n Invalid number, try again");
+                }
+            }
+
+
         }
 
         static void showStudents(Student st)
@@ -46,11 +101,8 @@ namespace My_Frist_Code
         }
 
         static bool searchForAStudent(int id ,string studName,Student stud) 
-        {  
-            bool found=false;
-            if (stud.name == studName && stud.ID == id ) found=true;
-          
-            return found;
+        { 
+            return stud.name == studName && stud.ID == id;
         }
         
         static void Main(string[] args)
@@ -58,20 +110,21 @@ namespace My_Frist_Code
             Student[] students = new Student[100];
             int numOfStudents = 0;
             int selectionNumber;
+            
 
             do 
             {
                 Console.WriteLine("\n    =====  Welcome  =====   ");
                 Console.WriteLine("   -----------------------   ");
                 Console.WriteLine("\n 1. Add Student \n 2. View All \n 3. Search \n 4. Update \n 5. Delete \n 6. Exit");
-                
-                Console.Write("\n Enter your selection number : ");
-                selectionNumber = int.Parse(Console.ReadLine());
-                
+     
+                selectionNumber = ReadInt(" \n Enter your selection number (choose from 1 to 6 ) : "); 
+                     
+                  
                 if(selectionNumber == 1)  // Add Student operation
                 {
                     students[numOfStudents] = new Student();
-                    addStudent(students[numOfStudents]);
+                    addStudent(students[numOfStudents], students, numOfStudents);
                     numOfStudents++;
                 }
 
@@ -87,8 +140,8 @@ namespace My_Frist_Code
 
                 else if (selectionNumber == 3)  // Search operation
                 { 
-                    Console.Write($"\n Student ID : ");
-                    int studID = int.Parse(Console.ReadLine());
+                    
+                    int studID = ReadInt("\n Student ID : "); 
 
                     Console.Write($"\n Student Name : ");
                    
@@ -105,13 +158,12 @@ namespace My_Frist_Code
                          }
               
                     }
-                    if(!found) Console.WriteLine("Not found"); 
+                    if(!found) Console.WriteLine("\nNot found"); 
                 }
 
                 else if (selectionNumber == 4)  // Update operation
                 { 
-                    Console.Write($"\n Student ID : ");
-                    int studID = int.Parse(Console.ReadLine());
+                    int studID = ReadInt("\n Student ID : ");
 
                     Console.Write($"\n Student Name : ");
                     string studName = Console.ReadLine();
@@ -120,7 +172,7 @@ namespace My_Frist_Code
                     {
                         if (searchForAStudent(studID, studName, students[i]))
                         {
-                           addStudent(students[i]);
+                           addStudent(students[i], students, numOfStudents);
                            break;
                         }
 
@@ -129,27 +181,31 @@ namespace My_Frist_Code
 
                 else if (selectionNumber == 5)  // Delete operation
                 {
-                    Console.Write($"\n Student ID : ");
-                    int studID = int.Parse(Console.ReadLine());
+                    int studID = ReadInt("\n Student ID : ");
+                    bool deleted=false;
 
                     Console.Write($"\n Student Name : ");
                     string studName = Console.ReadLine();
 
-                    for (int i = 0; i < numOfStudents-1 ; i++)
+                    for (int i = 0; i < numOfStudents -1 ; i++)
                     {
                         if (searchForAStudent(studID, studName, students[i]))
                         {
-                            while (i < numOfStudents) 
+                            while (i < numOfStudents -1) 
                             {
                                 students[i] = students[i + 1];
                                 i++;
                             }
                             students[numOfStudents-1] = null;
+                            deleted = true;
                             break;
                         }
 
                     }
-                    numOfStudents--;
+                    if(deleted)
+                        numOfStudents--;
+                    else
+                        Console.WriteLine("\nNot Found");
                 }
 
 
@@ -159,3 +215,27 @@ namespace My_Frist_Code
     }
 }
 
+
+/*
+            double side = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("The Area of the square : {0}", side * side);
+
+
+            double triangleBase = int.Parse(Console.ReadLine());
+            double triangleHeight = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The Area of the triangle : {0}", 0.5 * triangleBase * triangleHeight);
+            */
+
+            /*
+            Console.WriteLine("What is your name ?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your age ?");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is your country ?");
+            string country = Console.ReadLine();
+            Console.WriteLine("How meny member in your family ?");
+            int fm = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Your data :\n your name: {name}\n your age: {age}\n your county: {country}\n no of family member: {fm}");
+        */
